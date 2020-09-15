ceeb009adf575963a0993ec69f0188dd81fb1669
Su-Yong <simssy2205@gmail.com>
2020-09-15 19:17:52 +0900

    add more test

--- packages/scope-validator-core/test/index.spec.ts
+++ packages/scope-validator-core/test/index.spec.ts
@@ -2,23 +2,29 @@ import ScopeValidatorManager from '../lib/scope-validator-manager';
 import ScopeValidatorFactory from '../lib/scope-validator-factory';
 import ScopeValidator from '../lib/scope-validator';
 
+const TestValidator1 = ScopeValidatorFactory.create(
+  // eslint-disable-next-line no-template-curly-in-string
+  'create:client:${restricter}',
+  (name, { parameters, received }) => {
+    const { restricter } = parameters ?? { restricter: null };
+
+    if (restricter === 'all') return true;
+    if (restricter === 'me') return received?.ownerId === 'asdf';
+    return false;
+  }
+);
+
+const TestValidator2 = ScopeValidatorFactory.create(
+  'create:client:all',
+  (name, { parameters, received }) => {
+    return false;
+  }
+);
+
 describe('success', () => {
-  const TestValidator = ScopeValidatorFactory.create(
-    // eslint-disable-next-line no-template-curly-in-string
-    'create:client:${restricter}',
-    (name, { parameters, received }) => {
-      console.log(name, parameters, received);
-      const { restricter } = parameters ?? { restricter: null };
-
-      if (restricter === 'all') return true;
-      if (restricter === 'me') return received?.ownerId === 'asdf';
-      return false;
-    }
-  );
-
-  it('validate', () => {
+  it('check parameter', () => {
     const scopeValidatorManager = new ScopeValidatorManager();
-    scopeValidatorManager.use(TestValidator);
+    scopeValidatorManager.use(TestValidator1);
     scopeValidatorManager.context = {
       ownerId: 'asdf',
     };
@@ -30,5 +36,40 @@ describe('success', () => {
     );
 
     expect(result).toEqual([true, true, false]);
+  });
+});
+
+describe('failed', () => {
+  it('not match ownerId', () => {
+    const scopeValidatorManager = new ScopeValidatorManager();
+    scopeValidatorManager.use(TestValidator1);
+    scopeValidatorManager.context = {
+      ownerId: 'ss',
+    };
+
+    const result = scopeValidatorManager.validate(
+      'create:client:all',
+      'create:client:me',
+      'create:client:other'
+    );
+
+    expect(result).toEqual([true, false, false]);
+  });
+
+  it('multiple validator', () => {
+    const scopeValidatorManager = new ScopeValidatorManager();
+    scopeValidatorManager.use(TestValidator1);
+    scopeValidatorManager.use(TestValidator2);
+    scopeValidatorManager.context = {
+      ownerId: 'asdf',
+    };
+
+    const result = scopeValidatorManager.validate(
+      'create:client:all',
+      'create:client:me',
+      'create:client:other'
+    );
+
+    expect(result).toEqual([false, true, false]);
   });
 });
