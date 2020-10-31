bab9cc2ab4412eb2e2e8b15ad6f610c0387cc888
Su-Yong <simssy2205@gmail.com>
2020-10-31 23:49:59 +0900

    add async function

--- packages/scope-validator/lib/scope-validator-manager.ts
+++ packages/scope-validator/lib/scope-validator-manager.ts
@@ -11,6 +11,38 @@ export default class ScopeValidatorManager<T> {
     this.scopeValidators.push(...scopeValidators);
   }
 
+  async validateAsync(scope: string, context?: T): Promise<boolean>;
+  async validateAsync(scopes: string[], context?: T): Promise<boolean[]>;
+
+  async validateAsync(
+    scope: string | string[],
+    context?: T
+  ): Promise<boolean | boolean[]> {
+    if (scope instanceof Array) {
+      return Promise.all(
+        scope.map(async (one) => this.validateAsync(one, context))
+      );
+    }
+
+    const matchValidators = this.scopeValidators.filter((validator) =>
+      validator.canValidate(scope)
+    );
+
+    let result = true;
+    await Promise.all(
+      matchValidators.map(async (validator) => {
+        const isCheck = await validator.validate(scope, {
+          received: context,
+          parameters: validator.pattern.getParameters(scope),
+        });
+
+        if (result && !isCheck) result = false;
+      })
+    );
+
+    return matchValidators.length >= 1 && result;
+  }
+
   validate(scope: string, context?: T): boolean;
   validate(scopes: string[], context?: T): boolean[];
 
